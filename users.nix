7c3bf0b26bf82b0726094c02e401b3ae554d400f
Your Name <you@example.com>
2024-05-22 00:28:45 +0200

    1

--- users.nix
+++ users.nix
@@ -5,13 +5,13 @@
 }: {
   users.users = {
     "${config._module.args.guestUserName}" = {
-      id = 1001;
+      uid = 1001;
       extraGroups = ["networkmanager"];
       hashedPassword = "";
       isNormalUser = true;
     };
     "${config._module.args.powerUserName}" = {
-      id = 1000;
+      uid = 1000;
       extraGroups = [
         "wheel"
         "networkmanager"
@@ -21,16 +21,20 @@
       isNormalUser = true;
     };
   };
-  home-manager.users."${config._module.args.guestUserName}".home.stateVersion = "${config.system.stateVersion}";
-  home-manager.users."${config._module.args.guestUserName}".dconf.settings = {
-    "com/solus-project/budgie-panel".dark-theme = true;
-    "org/cinnamon/desktop/interface".gtk-theme = "Mint-Y-Dark-Aqua";
-    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    "org/gnome/mutter".dynamic-workspaces = true;
-    "org/gnome/mutter".edge-tiling = true;
-    "org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-type = "nothing";
-    "org/x/apps/portal".color-scheme = "prefer-dark";
+  home-manager.users."${config._module.args.guestUserName}" = {
+    home.stateVersion = "${config.system.stateVersion}";
+    dconf.settings = {
+      "com/solus-project/budgie-panel".dark-theme = true;
+      "org/cinnamon/desktop/interface".gtk-theme = "Mint-Y-Dark-Aqua";
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/mutter".dynamic-workspaces = true;
+      "org/gnome/mutter".edge-tiling = true;
+      "org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-type = "nothing";
+      "org/x/apps/portal".color-scheme = "prefer-dark";
+    };
   };
-  home-manager.users."${config._module.args.powerUserName}".home.stateVersion = "${config.system.stateVersion}";
-  home-manager.users."${config._module.args.powerUserName}".home.file.".ssh".source = "${config._module.args.powerUserHome}/.ssh";
+  home-manager.users."${config._module.args.powerUserName}".home = {
+    stateVersion = "${config.system.stateVersion}";
+    file.".ssh".source = "${config._module.args.powerUserHome}/.ssh";
+  };
 }
