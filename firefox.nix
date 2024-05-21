04bc5b1860e23f17de37f8cf926d00c892ac3175
tromshusky <tromshusky@gmail.com>
2024-05-21 18:31:32 +0200

    Update firefox.nix
    fixed type in varname

--- firefox.nix
+++ firefox.nix
@@ -1,7 +1,7 @@
 {...}: {
   programs.firefox = {
     enable = true;
-    policies.Extension.Install = [
+    policies.Extensions.Install = [
       "https://addons.mozilla.org/firefox/downloads/latest/i-dont-care-about-cookies/latest.xpi"
       "https://addons.mozilla.org/firefox/downloads/latest/norsk-bokmål-ordliste/latest.xpi"
       "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
