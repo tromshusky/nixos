d90c5320744d9b2869ffe670b3edfbe29e3a2e33
tromshusky <tromshusky@gmail.com>
2024-05-21 10:39:10 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.home-manager.url = github:nix-community/home-manager;
+
+  outputs = { self, nixpkgs, ... }@attrs: {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
