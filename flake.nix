fbafa1d5229f6de6eab6ffe9626656f525de9b23
Your Name <you@example.com>
2024-05-22 13:05:39 +0200

    revert

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.home-manager.url = github:nix-community/home-manager;
+
   outputs = { self, nixpkgs, ... }@attrs: {
-    confi = {
-      import = ./configuration.nix;
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./configuration.nix ];
     };
   };
 }
+
+
 
