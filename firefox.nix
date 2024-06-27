cc05c567e1bcfa481fb99176d563af6b78c9ee40
Your Name <you@example.com>
2024-06-27 23:31:00 +0200

    fireox:disabled translate popup

--- firefox.nix
+++ firefox.nix
@@ -1,10 +1,16 @@
-{...}: {
+{ ... }:
+{
   programs.firefox = {
     enable = true;
-    policies.Extensions.Install = [
-      "https://addons.mozilla.org/firefox/downloads/latest/i-dont-care-about-cookies/latest.xpi"
-      "https://addons.mozilla.org/firefox/downloads/latest/norsk-bokmål-ordliste/latest.xpi"
-      "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
-    ];
+    policies = {
+      Extensions.Install = [
+        "https://addons.mozilla.org/firefox/downloads/latest/i-dont-care-about-cookies/latest.xpi"
+        "https://addons.mozilla.org/firefox/downloads/latest/norsk-bokmål-ordliste/latest.xpi"
+        "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
+      ];
+      Preferences = {
+        "browser.translations.automaticallyPopup".Value = false;
+      };
+    };
   };
 }
