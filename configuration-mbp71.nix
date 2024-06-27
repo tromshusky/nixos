a032064167bd654b28b1ab3f5f02abf2411efaad
Your Name <you@example.com>
2024-06-27 23:28:53 +0200

    enabled keyboard backlight by default on macbook7-1

--- configuration-mbp71.nix
+++ configuration-mbp71.nix
@@ -12,6 +12,10 @@
   ];
   services.xserver.xkb.layout = "no,us";
   services.xserver.xkb.variant = "mac,";
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", RUN+="${pkgs.bash}/bin/bash -c 'echo 200 > /sys/class/leds/smc\:\:kbd_backlight/brightness'"
+  '';
+
   system.autoUpgrade = {
     enable = true;
     flags = [ "--no-write-lock-file" ];
