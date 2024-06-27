d8600d7428644f60ed01840b9c6ea70aefab3193
Your Name <you@example.com>
2024-06-27 23:41:33 +0200

    enabled night light by time

--- users.nix
+++ users.nix
@@ -21,17 +21,19 @@
   home-manager.users."${config._module.args.guestUserName}" = {
     home.stateVersion = "${config.system.stateVersion}";
     dconf.settings = {
-      "org/gnome/shell".enabled-extensions = [
-        ""
-        "dash-to-dock@micxgx.gmail.com"
-      ];
-      "org/gnome/shell".favorite-apps = [ "firefox.desktop" ];
       "com/solus-project/budgie-panel".dark-theme = true;
       "org/cinnamon/desktop/interface".gtk-theme = "Mint-Y-Dark-Aqua";
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
       "org/gnome/mutter".dynamic-workspaces = true;
       "org/gnome/mutter".edge-tiling = true;
+      "org/gnome/settings-daemon/plugins/color".night-light-enabled = true;
+      "org/gnome/settings-daemon/plugins/color".night-light-schedule-automatic = false;
       "org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-type = "nothing";
+      "org/gnome/shell".enabled-extensions = [
+        ""
+        "dash-to-dock@micxgx.gmail.com"
+      ];
+      "org/gnome/shell".favorite-apps = [ "firefox.desktop" ];
       "org/x/apps/portal".color-scheme = "prefer-dark";
     };
   };
