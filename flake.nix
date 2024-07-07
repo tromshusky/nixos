cb236f40ab0d81e806c87aa9457744187edf45a9
Your Name <you@example.com>
2024-07-07 13:08:48 +0200

    changed home-manager to release-24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
+
+#  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+#  inputs.home-manager.url = github:nix-community/home-manager;
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
-  inputs.home-manager.url = github:nix-community/home-manager;
+  inputs.home-manager.url = github:nix-community/home-manager/release-24.05;
+
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, ... }@attrs: {
