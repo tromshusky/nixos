82fc7f0d2918f1b22ee00e808625f6ab2e22457a
Your Name <you@example.com>
2024-07-07 14:12:57 +0200

    set gdm default

--- default-specialisation.nix
+++ default-specialisation.nix
@@ -2,6 +2,7 @@
   config = lib.mkIf (config.specialisation != {}) {
 		services.xserver.enable = true;
 		services.xserver.desktopManager.gnome.enable = true;
+		services.xserver.displayManager.gdm.enable = true;
 		services.gnome.core-utilities.enable = false;
 		environment.gnome.excludePackages = [ pkgs.gnome-tour ];
 		services.xserver.excludePackages = [ pkgs.xterm ];
