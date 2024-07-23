227f412a3c31c3c367a113a085d28b088dd38f60
Your Name <you@example.com>
2024-07-23 04:09:45 +0200

    jaja

--- noflake/configuration.nix
+++ noflake/configuration.nix
@@ -11,7 +11,8 @@ let
     // users
     // amsterdam
     // backup
-    // flakes {
+    // flakes
+    // {
       services.xserver.enable = lib.mkDefault true;
       services.xserver.desktopManager.gnome.enable = lib.mkDefault true;
       services.xserver.displayManager.gdm.enable = lib.mkDefault true;
